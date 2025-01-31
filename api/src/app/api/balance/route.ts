19cb098f2f6d47b5aa1c403eb40d5bfdd7a35da8
JD <jann.driessen@gmx.de>
2025-01-31 18:08:07 -0500

    add fetching cbbtc balance

--- api/src/app/api/balance/route.ts
+++ api/src/app/api/balance/route.ts
@@ -1,4 +1,27 @@
-export async function GET(request: Request) {
-  // TODO:
-  return Response.json({ address: "0x0", balance: "1000000000000000000" });
+import type { NextRequest } from "next/server";
+import { createPublicClient, http, isAddress, parseAbi } from "viem";
+import { base } from "viem/chains";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const account = searchParams.get("account");
+  if (!account || !isAddress(account)) {
+    return Response.json({ error: "invalid account address" }, { status: 400 });
+  }
+  const client = createPublicClient({
+    chain: base,
+    transport: http(process.env.ALCHMEY_API_KEY),
+  });
+  const data = await client.readContract({
+    address: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf", // cbBTC
+    abi: parseAbi(["function balanceOf(address owner) view returns (uint256)"]),
+    functionName: "balanceOf",
+    args: [account],
+  });
+  return Response.json({
+    account,
+    symbol: "cbBTC",
+    decimals: 8,
+    balance: data.toString(),
+  });
 }

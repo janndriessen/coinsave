17c6df29284f35c6e8b1bbb8db7d64c399610574
JD <jann.driessen@gmx.de>
2025-01-31 19:30:46 -0500

    add empty app

---
+++ app/coinsave/coinsaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  coinsaveApp.swift
+//  coinsave
+//
+//  Created by Jann Driessen on 31.01.25.
+//
+
+import SwiftUI
+
+@main
+struct coinsaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app/coinsaveTests/coinsaveTests.swift
@@ -0,0 +1,16 @@
+//
+//  coinsaveTests.swift
+//  coinsaveTests
+//
+//  Created by Jann Driessen on 31.01.25.
+//
+
+import Testing
+
+struct coinsaveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

9c413e0376096797b6b02ffb5f942abbd2ea0533
JD <jann.driessen@gmx.de>
2025-02-04 08:24:41 -0500

    update to post

--- api/src/app/api/account/route.ts
+++ api/src/app/api/account/route.ts
@@ -1,4 +1,6 @@
-export async function GET() {
+export async function POST(req: Request) {
+  const request = await req.json();
+  console.log(request);
   // TODO: create/init agent(s)
   // TODO: return account address
   return Response.json({ message: "created", account: "0x0" });

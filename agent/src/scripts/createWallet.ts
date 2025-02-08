6502f321ca4748a061918a1a717b1926813182ee
markus <33206087+markus0x1@users.noreply.github.com>
2025-02-08 16:49:26 +0100

    feat: create wallet

---
+++ agent/src/scripts/createWallet.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+import { validateEnvironment } from '../utils/config';
+import { createWallet } from '../utils/wallet';
+
+dotenv.config();
+validateEnvironment();
+
+async function main() {
+
+  createWallet();
+
+
+}
+
+if (require.main === module) {
+  console.log("Starting Agent...");
+  main().catch(error => {
+    console.error("Fatal error:", error);
+    process.exit(1);
+  });
+}
